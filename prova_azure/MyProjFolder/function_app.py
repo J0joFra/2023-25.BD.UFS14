34516902446c7f275df63823ea3dabfac06a9479
Joaquim Francalanci <94995228+J0joFra@users.noreply.github.com>
2024-10-28 15:55:14 +0000

    qr_code_svg

--- prova_azure/MyProjFolder/function_app.py
+++ prova_azure/MyProjFolder/function_app.py
@@ -1,6 +1,4 @@
 import azure.functions as func
-import datetime
-import json
 import logging
 import os
 
@@ -10,14 +8,14 @@ app = func.FunctionApp()
 def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    # Carica l'immagine dal file system
-    image_path = os.path.join(os.getcwd(), r"C:\Users\JoaquimFrancalanci\QR_code.svg")  # Modifica con il nome del file immagine
+    # Utilizza un percorso relativo per l'immagine
+    image_path = os.path.join(os.path.dirname(__file__), "QR_code.svg")
     try:
         with open(image_path, "rb") as image_file:
             image_data = image_file.read()
         return func.HttpResponse(
             image_data,
-            mimetype=r"C:\Users\JoaquimFrancalanci\QR_code.svg",
+            mimetype="image/svg+xml",  # Specifica il mimetype corretto per SVG
             status_code=200
         )
     except FileNotFoundError:
