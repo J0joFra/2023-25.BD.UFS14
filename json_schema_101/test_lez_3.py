4b7fe9a28d7812c4f711e5307ae1bfeb1f4e79b9
Joaquim Francalanci <94995228+J0joFra@users.noreply.github.com>
2024-12-02 08:23:03 +0000

    test update

--- json_schema_101/test_lez_3.py
+++ json_schema_101/test_lez_3.py
@@ -1,46 +1,41 @@
 from jsonschema import validate
 schema = {
-    "type" : "object",
-    "properties" : {
-        "price" : {"type" : "number"},
-        "name" : {"type" : "string"},
+    "type": "object",
+    "properties": {
+        "price": {"type": "number"},
+        "name": {"type": "string"},
     },
 }
-# content of text tample.py
+
+# content of test_template.py
 def func(x):
-    return x +1
+    return x + 1
 
 def test_answer():
     assert func(3) == 4
 
 def test_jsonschema_invalid():
-    assert validate_wrapper(instance={"name" : "Eggs", "price" : 34.99}, schema=schema) == True
+    assert validate_wrapper(instance={"name": "Eggs", "price": 34.99}, schema=schema) == True
 
 def validate_wrapper(instance, schema):
     try:
-        validate(instance = instance, schema = schema)
+        validate(instance=instance, schema=schema)
         return True
     except:
         return False
 
-
 def test_function_output_with_snapshot(snapshot):
     snapshot.snapshot_dir = 'snapshots'  # This line is optional.
-    test_func=func(5)
-    test_func_stringa=str(test_func)
-    snapshot.assert_match(test_func_stringa, 'foo_output.txt') 
+    test_func = func(5)
+    test_func_stringa = str(test_func)
+    snapshot.assert_match(test_func_stringa, 'foo_output.txt')
 
-
-
-frutti = """frutti,prezzo,colore,sapore
-pera,75,verde,buono
-mela,30,rosso,gucci
-ananas,13,giallo, schifo
+veicoli = """veicolo,prezzo,colore,tipo
+auto,20000,rosso,berlina
+moto,15000,nero,sportiva
+bicicletta,500,verde,urbana
 """
-
 
 def test_function_output_with_snapshot_csv(snapshot):
     snapshot.snapshot_dir = 'snapshots'  # This line is optional.
-    snapshot.assert_match(frutti, 'frutti.csv') 
-
-
+    snapshot.assert_match(veicoli, 'veicoli.csv')
